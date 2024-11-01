c608330ed077ab30860a09c7d0cbf969342fde95
Kason Steiner <kasonsteiner27@gmail.com>
2024-11-01 06:06:16 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Number-Guessing-Game/RandGuessGame.cpp
@@ -0,0 +1,35 @@
+/*
+Title: Number Guessing Game
+File Name:RandGuessGame.cpp
+Programmer: Kason Steiner
+Date: 10/2024
+Write a program that generates a random number and asks the user to guess what the number is. If the user’s guess is higher than the random number, the program should display “Too high, try again.” If the user’s guess is lower than the random number, the program should display “Too low, try again.” The program should use a loop that repeats until the user correctly guesses the random number.
+*/  
+
+
+#include <iostream>
+#include <random>
+
+using namespace std;
+int main() { 
+
+  int guess = 0;
+  random_device myEngine;
+  uniform_int_distribution<int> randomInt(1, 100);
+  int answer = randomInt(myEngine);
+
+  while (guess != answer)
+    {
+    cout << "Guess a number between 1 and 100: ";
+    cin >> guess;
+
+    if(guess > answer)
+      cout << "Too high, try again.\n";
+
+    if(guess < answer)
+      cout << "Too low, try again.\n";
+    }  
+  cout << "You got it!";
+
+return 0;
+}
